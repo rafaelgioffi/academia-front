a5ed37cb8af3befd946ed1f702de80a1a0a63aa5
Rafael Gioffi <rafaelgioffi@gmail.com>
2025-10-24 21:05:38 -0300

    teste

--- v2/src/pages/Projects/schemas.js
+++ v2/src/pages/Projects/schemas.js
@@ -1,7 +1,7 @@
 import * as yup from 'yup'
 
 export const validateForm = yup.object().shape({
-    title: yup.string().required("Campo obrigatório"),
+    title: yup.string(),
     value: yup.number("Apenas números aqui").required("Valor obrigatório"),
     description: yup.string().required("Campo obrigatório"),
 })
